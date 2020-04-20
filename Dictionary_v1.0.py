f01ad9084d1e23db303752a9743d288023e8d974
DanielReid-Uk <64040886+DanielReid-Uk@users.noreply.github.com>
2020-04-21 06:19:58 +0800

    Dictionary V1.0 Full release
    Dictionary V1.0 FULL RELEASE
     - Cleaned definition output to allow easier reading.
    
    *MAY ADD TO THIS PROJECT AT A LATER DATE*

---
+++ Dictionary_v1.0.py
@@ -0,0 +1,30 @@
+import json
+from difflib import get_close_matches # Allows us to compare likeness of 'word' with 'data.json'
+
+data = json.load(open('data.json'))
+
+def translate(word):
+    # Function 'translate' takes the word variable which 
+    # is found in line 16 and searches for it in 'data'. 
+    word=word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+        # If word can not be found first time around and 'get_close_matches' has 
+        # at least one likely match, then run this statement. 
+        answer=input('Did you mean %s instead? enter Y/N: ' %get_close_matches(word, data.keys())[0])
+        if answer.lower() == 'y':
+            return data[get_close_matches(word, data.keys())[0]]
+        else:
+         return 'This word you wanted does not exist!'   
+    else:
+        return 'This word does not exist!'
+
+word = input('Enter word: ')
+output=translate(word)
+
+if type(output) == list:
+    for definition in output:
+        print(definition)
+else:
+    print(output)
